fb74bb697fe4e04b52a21c18d3d0e39a40b55f1a
Skhmt <Skhmt@users.noreply.github.com>
2025-04-10 15:25:23 -0700

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,25 @@ export default defineConfig({
 		svelte(),
 		VitePWA({
 			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon-512x512.svg', 'ng.png'],
+			manifest: {
+				name: 'Catculator',
+				short_name: 'Catculator',
+				description: 'Tools for long range shooters',
+				theme_color: '#242424',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
 		})
 	],
 	base: './'
